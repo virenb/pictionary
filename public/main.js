0a4e058d9f27c0ce545575dc7eca7f0fff980781
Viren Bhagat <virenb@gmail.com>
2016-09-10 17:57:27 -0400

    Updated main.js to take user guesses

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var pictionary = function() {
+	var userName = prompt("What's your name?");
 	var socket = io();
 	var canvas, context;
 
@@ -9,6 +10,28 @@ var pictionary = function() {
 		context.fill();
 	};
 	var isMousedown = false;
+
+	// Listening for key presses in the input
+	var guessBox;
+	var userguess;
+	var onKeyDown = function(event) {
+	    if (event.keyCode != 13) { // Enter
+	        return;
+	    }
+	    var userguess = userName + "'s Guess: " + guessBox.val();
+	    console.log(userguess);
+	    socket.emit('guess', userguess);
+	    $('#usersGuess').text(userguess);
+	    guessBox.val('');
+	};
+
+	guessBox = $('#guess input');
+	guessBox.on('keydown', onKeyDown);
+	socket.on('guess', function (stuff) {
+		$('#usersGuess').text(stuff);
+	});
+	// End of listening section
+	
 	canvas = $('canvas');
 	context = canvas[0].getContext('2d');
 	canvas[0].width = canvas[0].offsetWidth;
--- server.js
+++ server.js
@@ -8,14 +8,31 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
-io.on('connect', function(socket) {
-	
+var users = [];
+var count = 0;
+
+io.on('connection', function(socket) {
+	count++;
+
+	io.sockets.on('connect', function(user) {
+    users.push(user); 
+    console.log('User is connected');
+	});	
+
 	socket.on('position', function(position) {
 		console.log('Is drawing')
 		socket.broadcast.emit('position', position);
 		console.log(position);
+		console.log('User is drawing');
 	});
-	
+
+	socket.on('guess', function(guess) {
+		socket.broadcast.emit('guess', guess);
+		console.log(guess);
+	});
+	// socket.on('draw', function(){
+	// 	console.log('User is drawing');
+	// });
 
 
 });
